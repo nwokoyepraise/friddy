b2b6bb549152149c8ad97ce9800d3f7ebbffdc97
nwokoyepraise <nwokoyepraise@gmail.com>
2021-09-07 21:59:15 +0100

    rename file

--- src/app.js
+++ src/app.js
@@ -4,13 +4,13 @@ const app = express();
 const server = require('http').createServer(app);
 const port_number = 3000;
 const user_reg = require('./routes/user_reg');
-const user_auth = require('./routes/user_auth');
+const user_login = require('./routes/user_login');
 
 app.use(express.json());
 
 //load routes
 app.use('/api/user_reg', user_reg);
-app.use('/api/user_login', user_auth);
+app.use('/api/user_login', user_login);
 
 server.listen(port_number, function () {
     console.log(`server listening on port ${port_number}`);
