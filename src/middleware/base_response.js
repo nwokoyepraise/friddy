47640ae35c727fb7c165318485bbd6f15b85895b
nwokoyepraise <nwokoyepraise@gmail.com>
2021-09-08 21:13:07 +0100

    middleware to handle responses

---
+++ src/middleware/base_response.js
@@ -0,0 +1,13 @@
+module.exports = function (req, res) {
+    let data = res.locals.data;
+    //revert response to user
+    switch (data.status) {
+        case false:
+            res.status(data.status_code).send({ status: false, message: data.message });
+            break;
+
+        case true:
+            res.status(200).send({ status: true, data: data.data });
+            break;
+    }
+}
