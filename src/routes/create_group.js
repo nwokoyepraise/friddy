32f60db5f9ae293dbb34e4697503aa385bd5986c
nwokoyepraise <nwokoyepraise@gmail.com>
2021-09-08 21:15:29 +0100

    implement route to create groups

---
+++ src/routes/create_group.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+const group_handler = require('../services/group_handler');
+
+module.exports = router.post('', async function (req, res, next) {
+    try {
+        res.locals.data = await group_handler.create_group(req.body, req.user);
+        //revert response to user 
+        next();
+
+    } catch (error) {
+        console.error(error);
+    }
+});
