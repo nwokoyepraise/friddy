fbcc6230fd2da4c677c0a9913961ebc629b4afd1
nwokoyepraise <nwokoyepraise@gmail.com>
2021-09-08 21:19:24 +0100

    add routes

--- src/app.js
+++ src/app.js
@@ -1,16 +1,21 @@
 require('dotenv').config();
+require('./middleware/jwt_auth');
 const express = require('express');
 const app = express();
 const server = require('http').createServer(app);
 const port_number = 3000;
 const user_reg = require('./routes/user_reg');
 const user_login = require('./routes/user_login');
+const create_group = require('./routes/create_group');
+const passport = require('passport');
+const base_response = require('./middleware/base_response');
 
 app.use(express.json());
 
 //load routes
-app.use('/api/user_reg', user_reg);
-app.use('/api/user_login', user_login);
+app.use('/api/user_reg', user_reg, base_response);
+app.use('/api/user_login', user_login, base_response);
+app.use('/api/groups/create_group', passport.authenticate('jwt', {session: false}) , create_group, base_response);
 
 server.listen(port_number, function () {
     console.log(`server listening on port ${port_number}`);
